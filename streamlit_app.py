56670209cbc043ab9f056d7e08f4e3791923e32f
Riza Masykur <hanirizo@gmail.com>
2025-05-11 14:17:03 +0000

    nyos

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,16 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.title("Orca Jobs 👋")
+st.markdown(
+    """ 
+    **There's :rainbow[so much] you can build!**
+    
+    ![orcalocal](https://cronitor.io/badges/zwT015/production/__qOVEnLVaqlR1u5UJZ1mDGg7MA.svg)
+    ![orcadev](https://cronitor.io/badges/CjvpbV/production/cFAI89JGPYsPJQxlu5RFSBNiG20.svg)
+    ![orcaproduction](https://cronitor.io/badges/gg5Cy6/production/elfKPoevzMKwv0W7dk3bs6ITFzs.svg)
+    ![orcatesting](https://cronitor.io/badges/dhEGPQ/production/lEAhPtxWq2ZnId4lRk3TgSStW60.svg)    
+    """
 )
+
+if st.button("Send balloons!"):
+    st.balloons()
